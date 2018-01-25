d0c72f5df063588e86771a3262fc8f9bbcfd00c8
Mario Negro <mario.negro.martin@gmail.com>
2018-01-25 09:14:37 +0100

    Traps SIGINT and removes partially generated images

--- Rakefile
+++ Rakefile
@@ -1,5 +1,10 @@
 require "bundler/gem_tasks"
 require "rake/testtask"
+require 'rubygems/package_task'
+
+spec = eval(File.read('retinator.gemspec'))
+Gem::PackageTask.new(spec) do |pkg|
+end
 
 Rake::TestTask.new(:test) do |t|
   t.libs << "test"
--- lib/retinator.rb
+++ lib/retinator.rb
@@ -1,5 +1,6 @@
 require "mini_magick"
 require "colorize"
+require "fileutils"
 require_relative "retinator/version"
 require_relative "retinator/config"
 require_relative "retinator/utils"
@@ -8,18 +9,31 @@ module Retinator
   class << self
     def generate(path, res)
       return false unless path && res
-      show_dimension_hint_if_needed(path, res)
-      name = Utils.filename(path)
+      show_dimension_hint_if_needed path, res
+      generate_images path, res
+      return true
+    end
+
+    def remove_files_on_abort
+      @saved_files.each do |file|
+        FileUtils.rm file
+      end
+      @saved_files.clear
+    end
+
+    private def generate_images(path, res)
+      @saved_files = []
+      name = Utils.filename path
       OUTPUTS.each do |config|
         scale = config[0]
         suffix = config[1]
         filename = "#{name}#{suffix}.#{OUTPUT_FORMAT}"
         puts "Generating #{filename}..."
-        image = resize(path, res * scale)
-        save_file(image, filename, OUTPUT_FORMAT)
+        image = resize path, res * scale
+        save_file image, filename, OUTPUT_FORMAT
       end
       puts "Done!".green
-      return true
+      @saved_files.clear
     end
 
     private def show_dimension_hint_if_needed(path, res)
@@ -32,13 +46,14 @@ module Retinator
     end
 
     private def resize(path, res)
-      image = MiniMagick::Image.open(path)
+      image = MiniMagick::Image.open path
       return image.resize("#{res}x#{res}^")
     end
 
     private def save_file(image, filename, format)
-      image.format(format)
-      image.write(filename)
+      image.format format
+      image.write filename
+      @saved_files << filename
     end
   end
 end
